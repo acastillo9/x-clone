78c524ef39a039bd15beedbee73789f18d261022
Andrés Castillo <info@acastillo.dev>
2024-04-20 11:34:04 -0500

    Add hover styles

--- src/Tweet.jsx
+++ src/Tweet.jsx
@@ -1,28 +1,36 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faRetweet, faChartSimple, faArrowUpFromBracket } from '@fortawesome/free-solid-svg-icons'
+import { faComment, faHeart, faBookmark } from '@fortawesome/free-regular-svg-icons'
+import dayjs from 'dayjs'
+
 function Tweet({ data }) {
+
+    function goToTweet(id) {
+        console.log('go to', id)
+    }
+
     return (
-        <article className="flex p-4 gap-4 w-full">
+        <article className="flex p-4 gap-4 w-full cursor-pointer" onClick={() => goToTweet(data.id)}>
             <div><img className="w-10 h-10 rounded-full" src={data.profileImage} alt={data.name} /></div>
             <div className="flex flex-col gap-1 flex-1">
                 <div className="flex gap-1">
-                    <a className="font-bold" href="#">{data.name}</a>
+                    <a className="font-bold hover:underline" href="#">{data.name}</a>
                     <a className="text-neutral-500" href="#">{data.user}</a>
                     <span className="text-neutral-500">·</span>
-                    <span className="text-neutral-500">{data.timestamp}</span>
+                    <span className="text-neutral-500">{dayjs(data.timestamp).format('MMM DD')}</span>
                 </div>
                 <div>
                     <p>{data.text}</p>
                 </div>
-                <div>
+                <div className='pt-2'>
                     <ul className="flex justify-between">
-                        <li><button>R {data.replays}</button></li>
-                        <li><button>P {data.reposts}</button></li>
-                        <li><button>L {data.likes}</button></li>
-                        <li><button>V {data.views}</button></li>
-                        <li>
-                            <div className="flex gap-2">
-                                <button>B</button>
-                                <button>S</button>
-                            </div>
+                        <li className='flex gap-2 items-center'><button className='text-neutral-500 hover:text-sky-600 transition'><FontAwesomeIcon icon={faComment} /> <span>{data.replays}</span></button></li>
+                        <li className='flex gap-2 items-center'><button className='text-neutral-500 hover:text-green-500 transition'><FontAwesomeIcon icon={faRetweet} /> <span>{data.reposts}</span></button></li>
+                        <li className='flex gap-2 items-center'><button className='text-neutral-500 hover:text-pink-600 transition'><FontAwesomeIcon icon={faHeart} /> <span>{data.likes}</span></button></li>
+                        <li className='flex gap-2 items-center'><button className='text-neutral-500 hover:text-sky-600 transition'><FontAwesomeIcon icon={faChartSimple} /> <span>{data.views}</span></button></li>
+                        <li className='flex gap-3 items-center'>
+                            <button className='text-neutral-500 hover:text-sky-600 transition'><FontAwesomeIcon icon={faBookmark} /></button>
+                            <button className='text-neutral-500 hover:text-sky-600 transition'><FontAwesomeIcon icon={faArrowUpFromBracket} /></button>
                         </li>
                     </ul>
                 </div>
